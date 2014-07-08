4459386594acefab8f209116d305bd09663265d8
nichalos <junfengbai2011@gmail.com>
2014-07-08 16:34:04 +0800

    nichalos fixed issue for setting

---
+++ LiveApp/LiveTextField.h
@@ -0,0 +1,13 @@
+//
+//  LiveTextField.h
+//  LiveApp
+//
+//  Created by Artron on 14-7-8.
+//  Copyright (c) 2014年 nichalos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LiveTextField : UITextField
+
+@end
